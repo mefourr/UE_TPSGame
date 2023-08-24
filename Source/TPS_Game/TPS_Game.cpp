c69cf2bc215270ce7a5c105ebc81cf47a7d8c732
Vyacheslav Olifirenko <122205134+mefourr@users.noreply.github.com>
2023-08-24 10:13:18 +0300

    Delete TPS_Game dir

--- Source/TPS_Game/TPS_Game.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TPS_Game.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, TPS_Game, "TPS_Game");
