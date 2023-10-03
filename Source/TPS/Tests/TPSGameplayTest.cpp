3b5b023e21d396925e72eba2146ab2e1e13caaa5
Jeff <mitchelll8@mail.ru>
2023-10-03 14:24:00 +0300

    jump input testing

--- Source/TPS/Tests/TPSGameplayTest.cpp
+++ Source/TPS/Tests/TPSGameplayTest.cpp
@@ -11,28 +11,65 @@
 #include "GameFramework/Character.h"
 #include "Components/InputComponent.h"
 
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(FInventroyItemCanBeTakenOnJump, "TPSGame.Gameplay.FInventroyItemCanBeTakenOnJump",
+#define UNDEFINEINDEX -1
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FInventroyItemCanBeTakenOnJump, "TPSGame.Gameplay.InventroyItemCanBeTakenOnJump",
+    EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority);
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FInventroyItemCantBeTakenOnJumpIfTooHigh, "TPSGame.Gameplay.InventroyItemCantBeTakenOnJumpIfTooHigh",
     EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority);
 
 using namespace TPS::Test;
 
+namespace
+{
+int32 GetActionBindingIndexByName(UInputComponent* InputComp, const FString& ActionName, const EInputEvent InputEvent)
+{
+    if (!InputComp) return UNDEFINEINDEX;
+    for (int32 i = 0; i < InputComp->GetNumActionBindings(); ++i)
+    {
+        const FInputActionBinding Action = InputComp->GetActionBinding(i);
+        if (Action.GetActionName().ToString().Equals(ActionName) && Action.KeyEvent == InputEvent)
+        {
+            return i;
+        }
+    }
+    return UNDEFINEINDEX;
+}
+}  // namespace
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FJumpLatentCommand, ACharacter*, Character);
+
+bool FJumpLatentCommand::Update()
+{
+    if (!Character) return false;
+    const int32 ActionIndex = GetActionBindingIndexByName(Character->InputComponent, "Jump", EInputEvent::IE_Pressed);
+    if (ActionIndex != UNDEFINEINDEX)
+    {
+        const auto JumpAction = Character->InputComponent->GetActionBinding(ActionIndex);
+        JumpAction.ActionDelegate.Execute(EKeys::SpaceBar);
+    }
+    return true;
+}
+
 bool FInventroyItemCanBeTakenOnJump::RunTest(const FString& Parameters)
 {
+    // /Game/Tests/InventroyTestLevel_2'
     const auto Level = LevelScope{"/Game/Tests/InventroyTestLevel_1"};
 
     UWorld* World = GetTestGameWorld();
     if (!TestNotNull("World exists", World)) return false;
 
-    ACharacter* Charcter = UGameplayStatics::GetPlayerCharacter(World, 0);
-    if (!TestNotNull("Charcter exists", Charcter)) return false;
+    ACharacter* Character = UGameplayStatics::GetPlayerCharacter(World, 0);
+    if (!TestNotNull("Charcter exists", Character)) return false;
 
     TArray<AActor*> InventroyItems;
     UGameplayStatics::GetAllActorsOfClass(World, ATPSInvetoryItem::StaticClass(), InventroyItems);
     if (!TestEqual("Only one item exists", InventroyItems.Num(), 1)) return false;
 
     ADD_LATENT_AUTOMATION_COMMAND(FWaitLatentCommand(1.0f));
-
     // Jump ...
+    ADD_LATENT_AUTOMATION_COMMAND(FJumpLatentCommand(Character));
 
     ADD_LATENT_AUTOMATION_COMMAND(FDelayedFunctionLatentCommand(
         [=]()
@@ -40,6 +77,35 @@ bool FInventroyItemCanBeTakenOnJump::RunTest(const FString& Parameters)
             TArray<AActor*> InventroyItems;
             UGameplayStatics::GetAllActorsOfClass(World, ATPSInvetoryItem::StaticClass(), InventroyItems);
             TestTrueExpr(InventroyItems.Num() == 0);
+        },
+        2.0f));
+    return true;
+}
+
+bool FInventroyItemCantBeTakenOnJumpIfTooHigh::RunTest(const FString& Parameters)
+{
+    const auto Level = LevelScope{"/Game/Tests/InventroyTestLevel_2"};
+
+    UWorld* World = GetTestGameWorld();
+    if (!TestNotNull("World exists", World)) return false;
+
+    ACharacter* Character = UGameplayStatics::GetPlayerCharacter(World, 0);
+    if (!TestNotNull("Charcter exists", Character)) return false;
+
+    TArray<AActor*> InventroyItems;
+    UGameplayStatics::GetAllActorsOfClass(World, ATPSInvetoryItem::StaticClass(), InventroyItems);
+    if (!TestEqual("Only one item exists", InventroyItems.Num(), 1)) return false;
+
+    ADD_LATENT_AUTOMATION_COMMAND(FWaitLatentCommand(1.0f));
+    // Jump ...
+    ADD_LATENT_AUTOMATION_COMMAND(FJumpLatentCommand(Character));
+
+    ADD_LATENT_AUTOMATION_COMMAND(FDelayedFunctionLatentCommand(
+        [=]()
+        {
+            TArray<AActor*> InventroyItems;
+            UGameplayStatics::GetAllActorsOfClass(World, ATPSInvetoryItem::StaticClass(), InventroyItems);
+            TestTrueExpr(InventroyItems.Num() == 1);
         },
         2.0f));
     return true;
